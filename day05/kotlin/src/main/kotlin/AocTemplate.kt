ce774bfcac1d40e375dac18e48493455fc172a53
Gustav Larson <gustav.larson@gmail.com>
2021-12-06 07:59:45 +0100

    Cleanup

--- day05/kotlin/src/main/kotlin/AocTemplate.kt
+++ day05/kotlin/src/main/kotlin/AocTemplate.kt
@@ -9,71 +9,18 @@ fun main() {
     println(answer)
 }
 
-fun solutionPart1(input: List<String>): Int {
+fun solutionPart1(input: List<String>) = solver(input, false)
+
+fun solutionPart2(input: List<String>) = solver(input, true)
+
+fun solver(input: List<String>, diagonal: Boolean): Int {
     val ocean = Array(1000) { Array(1000) { 0 } }
     val regex = Regex("(\\d+),(\\d+)\\s+->\\s+(\\d+),(\\d+)")
     input.forEach { line ->
         val (xx1, yy1, xx2, yy2) = regex.find(line)!!.destructured
-        var x1 = 0;
-        var x2 = 0;
-        var y1 = 0;
-        var y2 = 0;
-        if (xx1.toInt() < xx2.toInt()) {
-            x1 = xx1.toInt(); x2 = xx2.toInt()
-        } else {
-            x1 = xx2.toInt(); x2 = xx1.toInt()
-        }
-        if (yy1.toInt() < yy2.toInt()) {
-            y1 = yy1.toInt(); y2 = yy2.toInt()
-        } else {
-            y1 = yy2.toInt(); y2 = yy1.toInt()
-        }
-
-        if (x1 == x2) {
-            for (y in y1..y2) {
-                print(y)
-                ocean[y][x1]++
-            }
-            println("x1")
-        } else if (y1 == y2) {
-            for (x in x1..x2) {
-                print(x)
-                ocean[y1][x]++
-            }
-            println("x2")
-        } else {
-            println("diagonal")
-        }
-    }
-
-    var count = 0
-
-    var b = 0;
-    var c = 0;
-    for (row in ocean) {
-        b++
-        for (a in row) {
-            c++
-            if (a > 1) count++
-            print(a)
-            if (c > 9) break
-        }
-        c = 0
-        if (b > 9) break;
-        println()
-    }
-
-    return count
-}
-
-fun solutionPart2(input: List<String>): Int {
-    val ocean = Array(1000) { Array(1000) { 0 } }
-    val regex = Regex("(\\d+),(\\d+)\\s+->\\s+(\\d+),(\\d+)")
-    input.forEach { line ->
-        val (xx1, yy1, xx2, yy2) = regex.find(line)!!.destructured
-        val x1: Int = xx1.toInt();
+        val x1: Int = xx1.toInt()
         val x2: Int = xx2.toInt()
-        val y1: Int = yy1.toInt();
+        val y1: Int = yy1.toInt()
         val y2: Int = yy2.toInt()
 
         val dy: Int = if (yy1.toInt() < yy2.toInt()) {
@@ -84,18 +31,15 @@ fun solutionPart2(input: List<String>): Int {
 
         if (x1 == x2) {
             for (y in y1 toward y2) {
-                print(y)
                 ocean[y][x1]++
             }
         } else if (y1 == y2) {
             for (x in x1 toward x2) {
-                print(x)
                 ocean[y1][x]++
             }
-        } else {
+        } else if (diagonal) {
             var y = y1
             for (x in x1 toward x2) {
-                print("${x},${y} ")
                 ocean[y][x]++
                 y += dy
             }
@@ -104,11 +48,7 @@ fun solutionPart2(input: List<String>): Int {
 
     var count = 0
 
-    for (row in ocean) {
-        for (a in row) {
-            if (a > 1) count++
-        }
-    }
+    ocean.forEach { row -> row.forEach { if (it > 1) count++ } }
 
     return count
 }
