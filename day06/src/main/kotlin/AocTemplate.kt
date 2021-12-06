bfd439078574e8943824cb35edcc6637c81df0ae
Gustav Larson <gustav.larson@gmail.com>
2021-12-06 07:52:23 +0100

    Refactor

--- day06/src/main/kotlin/AocTemplate.kt
+++ day06/src/main/kotlin/AocTemplate.kt
@@ -9,32 +9,17 @@ fun main() {
     println(answer)
 }
 
-fun solutionPart1(fishes: List<Int>): Long {
-    var state = fishes
-    var nextState: MutableList<Int> = mutableListOf()
-    for (day in 1..80) {
-        nextState = mutableListOf()
-        for (age in state) {
-            if (age == 0) {
-                nextState.add(6)
-                nextState.add(8)
-            } else {
-                nextState.add(age - 1)
-            }
-        }
-        state = nextState
-        println("After day ${day}:") // ${state.joinToString()}")
-    }
-    return state.size.toLong()
-}
+fun solutionPart1(fishes: List<Int>)= simulator(fishes, 80)
 
-fun solutionPart2(fishes: List<Int>) : Long {
+fun solutionPart2(fishes: List<Int>) = simulator(fishes, 256)
+
+fun simulator(fishes: List<Int>, days: Int) : Long {
     var state = MutableList(9) { 0L }
     for (age in fishes) {
         state[age]++
     }
 
-    for (day in 1..256) {
+    for (day in 1..days) {
         val nextState = MutableList(9) { 0L }
         nextState[6] = state[0]
         nextState[8] = state[0]
